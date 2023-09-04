576745bbc300915b64e4c5fc108a189a50592fc0
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-09-04 07:58:10 +0530

    explore arrays and objects

--- basics.ts
+++ basics.ts
@@ -8,3 +8,14 @@ userName = "Max";
 
 let isInstructor: boolean;
 isInstructor = true;
+
+// Complex Types: arrays, objects
+
+let hobbies: string[];
+hobbies = ["Sports", "Cooking"];
+
+let person: { name: string; age: number };
+person = { name: "Max", age: 32 };
+//person = { isEmployee: true };
+
+let people: { name: string; age: number }[];
