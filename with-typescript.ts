c6f3de750a89024d2172f5352be44046fa1ec5c4
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-09-04 07:47:31 +0530

    compile ts file

--- with-typescript.ts
+++ with-typescript.ts
@@ -1,7 +1,7 @@
-function addNumbers(a: number, b: number) {
+function add(a: number, b: number) {
   return a + b;
 }
 
-const results = add(2, 5);
+const result = add(2, 5);
 
-console.log(results);
+console.log(result);
