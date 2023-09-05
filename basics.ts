27fa513f37b7c67ed4fbdef4b7a19c6efcf95c1f
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-09-05 08:27:18 +0530

    explore functions

--- basics.ts
+++ basics.ts
@@ -26,3 +26,13 @@ let people: Person[];
 
 let course: string | number = "React - The Complete Guide";
 course = 12341;
+
+// Functions & Types
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function printOutput(value: any) {
+  console.log(value);
+}
